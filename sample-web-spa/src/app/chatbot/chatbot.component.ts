abc993215d8f6c0bc3c05846fdb166db19ba43a7
Jaideep <jaideep_rajput@live.com>
2024-10-30 20:52:56 -0400

    Retrieve eoth Tools OpenAI

--- sample-web-spa/src/app/chatbot/chatbot.component.ts
+++ sample-web-spa/src/app/chatbot/chatbot.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 
+export interface IRetrieveWithTools {
+  output: string
+  actionLink: string
+  takeAction: boolean
+}
+
 @Component({
   selector: 'app-chatbot',
   standalone: true,
@@ -21,31 +27,45 @@ export class ChatbotComponent {
   sendMessage() {
     const userMessage = this.userMessage;
     this.chatMessages.push({ role: 'user', content: userMessage });
-    this.httpClient.post('http://localhost:8000/retrieve', {
+    this.httpClient.post('http://localhost:8000/retrieve_with_tools', {
         "data": userMessage
-    }).subscribe((response: any) => {
-      let isValidJson = true;
-      try {
-        response = JSON.parse(response);
-      } catch(e) {
-        isValidJson = false;
-      }
-      if (!isValidJson) {
-        response = response;
-      }
-      
-      if (response.actionLink) {
+      }).subscribe((response: any) => {
+      if (response.actionLink && response.takeAction) {
         this.assistantReply = "Sure!. Taking you to " + response.actionLink;
-      } else if(response.output) {
-        this.assistantReply = response.output;
       } else {
-        this.assistantReply = response;
+        this.assistantReply = response.output;
       }
       this.chatMessages.push({ role: 'assistant', content: this.assistantReply });
       this.userMessage = '';
-      if (response.actionLink) {
+      if (response.actionLink && response.takeAction) {
         this.router.navigateByUrl(response.actionLink);
       }
     });
+    // this.httpClient.post('http://localhost:8000/retrieve', {
+    //     "data": userMessage
+    // }).subscribe((response: any) => {
+    //   let isValidJson = true;
+    //   try {
+    //     response = JSON.parse(response);
+    //   } catch(e) {
+    //     isValidJson = false;
+    //   }
+    //   if (!isValidJson) {
+    //     response = response;
+    //   }
+      
+    //   if (response.actionLink && response.actionLink !== "") {
+    //     this.assistantReply = "Sure!. Taking you to " + response.actionLink;
+    //   } else if(response.output) {
+    //     this.assistantReply = response.output;
+    //   } else {
+    //     this.assistantReply = response;
+    //   }
+    //   this.chatMessages.push({ role: 'assistant', content: this.assistantReply });
+    //   this.userMessage = '';
+    //   if (response.actionLink) {
+    //     this.router.navigateByUrl(response.actionLink);
+    //   }
+    // });
   }
 }
