0fd18796b299ece0ddd8c802f3ac9a32ed1cda81
demo <demo@Ravikumars-Mac-mini.local>
2024-11-02 18:08:13 -0500

    new clientview

---
+++ sample-web-spa/src/app/client-views/client-accounts/client-accounts.component.ts
@@ -0,0 +1,13 @@
+// Client Accounts Component: Lists and manages client accounts and holdings.import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'client-accounts',
+    standalone: true,
+    imports: [],
+    templateUrl: './client-accounts.component.html',
+    styleUrl: './client-accounts.component.less'
+  })
+  export class ClientAccountsComponent {
+  
+  }
---
+++ sample-web-spa/src/app/client-views/client-overview/client-overview.component.ts
@@ -0,0 +1,13 @@
+// Client Overview Component: Shows client summary, profile, and goals.import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'client-overview',
+    standalone: true,
+    imports: [],
+    templateUrl: './client-overview.component.html',
+    styleUrl: './client-overview.component.less'
+  })
+  export class ClientOverviewComponent {
+  
+  }
