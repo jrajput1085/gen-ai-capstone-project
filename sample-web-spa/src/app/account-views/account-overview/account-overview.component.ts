0330d99305c912e9382c101d14fc064d42a7a3bf
demo <demo@Ravikumars-Mac-mini.local>
2024-11-02 18:10:13 -0500

    account

---
+++ sample-web-spa/src/app/account-views/account-overview/account-overview.component.ts
@@ -0,0 +1,13 @@
+// Account Overview Component: Provides balance, asset allocation, and transaction history.import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'account-overview',
+    standalone: true,
+    imports: [],
+    templateUrl: './account-overview.component.html',
+    styleUrl: './account-overview.component.less'
+  })
+  export class AccountOverviewComponent {
+  
+  }
